74dcd68b4f5d79302bbaf63598e0b9a8b41c7dc0
송다혜 <dada3254@daum.net>
2021-05-16 04:02:27 +0900

    refactor(InputView) : 자동차를 입력할때, 자동차로 리턴하는 메서드로 변경
    carsIn > readCars 함수명 및 리턴 변경, 기능 추가
    racingCntIn > readRacingCount 함수명 변경

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -3,10 +3,19 @@ package racingcar;
 import java.util.Scanner;
 
 public class InputView {
-    int racingCntIn(Scanner scanner) {
+    int readRacingCount(Scanner scanner) {
         return scanner.nextInt();
     }
-    String carsIn(Scanner scanner) {
-        return scanner.nextLine();
+
+    //자동차이름을 읽어 자동차를 리턴
+    Car[] readCars(Scanner scanner) {
+        String carNameAll = scanner.nextLine();
+        String[] carArr = carNameAll.split(",");
+        // 각 Car 객체를 담은 배열
+        Car[] cars = new Car[carArr.length];
+        for (int i = 0; i < carArr.length; i++) {
+            cars[i] = new Car(carArr[i]);
+        }
+        return cars;
     }
 }
