8594892545091fdb7f7095c3dc9780887897d51b
송다혜 <dada3254@daum.net>
2021-05-16 01:53:12 +0900

    feat(View) : View class 생성 및 주요메서드 추가

---
+++ src/main/java/racingcar/View.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+public class View {
+
+    void InputView() {}
+    void OutputView(Object obj) {
+        System.out.print(obj);
+    }
+    void EmptyView() {
+        System.out.println();
+    }
+
+}
