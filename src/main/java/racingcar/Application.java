ac36ac0fe17e9881e0ee6d3804d186bc480d0eb0
송다혜 <dada3254@daum.net>
2021-05-20 06:41:38 +0900

    refactor(sout) : InputView에 sout 임의로 넣기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -14,10 +14,9 @@ public class Application {
         // TODO 구현 진행
         OutputView outputView = new OutputView();
         InputView inputView = new InputView();
-        outputView.printMsg("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
         Car[] cars = inputView.readCars(scanner);
 
-        outputView.printMsg("시도할 회수는 몇 회인가요?");
         int racingCnt = inputView.readRacingCount(scanner);
 
         outputView.printMsg("\n실행결과");
--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -8,6 +8,7 @@ public class InputView {
     public static final int LIMIT_CAR_NAME = 5;
 
     int readRacingCount(Scanner scanner) {
+        System.out.println("시도할 회수는 몇 회인가요?");
         int racingCnt = 0;
         try {
             racingCnt = scanner.nextInt();
@@ -22,6 +23,7 @@ public class InputView {
 
     //자동차이름을 읽어 자동차를 리턴
     public Car[] readCars(Scanner scanner) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNameAll = scanner.nextLine();
         String[] carArr = carNameAll.split(",");
         checkCarName(carArr);
